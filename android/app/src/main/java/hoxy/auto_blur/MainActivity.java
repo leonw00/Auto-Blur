93f5389d22d1c10b44bbb8ac0ab9dc9d68ae2ba3
paxx dream <warsitol@myumanitoba.ca>
2021-10-27 14:29:59 -0500

    initialize project

---
+++ android/app/src/main/java/hoxy/auto_blur/MainActivity.java
@@ -0,0 +1,6 @@
+package hoxy.auto_blur;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: CupertinoColors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+      ),
+      body: Container(
+        child: Text("TEST"),
+      ),
+    );
+  }
+
+}
