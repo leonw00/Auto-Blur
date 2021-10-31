4fe1be8d4f3e8b4d0ecdb0303b0785e4d6a7c734
paxx dream <warsitol@myumanitoba.ca>
2021-10-31 17:58:36 -0500

    added base blocks for the core face detection

---
+++ lib/objects/painter.dart
@@ -0,0 +1,29 @@
+import 'dart:ui' as ui;
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Painter extends CustomPainter {
+
+  Painter(this.rect, this.image);
+
+  final List<Rect> rect;
+  ui.Image image;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint = Paint()
+      ..color = Colors.red
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 7;
+
+    canvas.drawImage(image, Offset.zero, paint);
+    for (var i = 0; i <= rect.length - 1; i++) {
+      canvas.drawRect(rect[i], paint);
+    }
+  }
+
+  @override
+  bool shouldRepaint(oldDelegate) {
+    return true;
+  }
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,10 @@
+import 'dart:math';
+import 'dart:ui' as ui;
+import 'package:auto_blur/objects/painter.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_ml_kit/google_ml_kit.dart';
+import 'package:image_picker/image_picker.dart';
 
 
 class HomeScreen extends StatefulWidget {
@@ -9,15 +14,79 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
 
+  final ImagePicker _picker = ImagePicker();
+  final faceDetector = GoogleMlKit.vision.faceDetector();
+  late ui.Image uiImage;
+
+
+  // ignore: unnecessary_statements, unnecessary_statements
+  Future pickImage() async {
+    // Pick an image
+    final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
+    final inputImage = InputImage.fromFilePath(image!.path);
+    
+    final List<Face> faces = await faceDetector.processImage(inputImage);
+    print('Found ${faces.length} faces');
+
+    List<Rect> rectArr = [];
+
+    for (Face face in faces) {
+      final Rect boundingBox = face.boundingBox;
+
+      final double? rotY = face.headEulerAngleY; // Head is rotated to the right rotY degrees
+      final double? rotZ = face.headEulerAngleZ; // Head is tilted sideways rotZ degrees
+
+      rectArr.add(face.boundingBox);
+    }
+
+    var bytesFromImageFile = inputImage.bytes;
+
+
+    decodeImageFromList(bytesFromImageFile!).then((img) {
+      setState(() {
+        uiImage = img;
+      });
+    });
+
+    FittedBox(
+      child: SizedBox(
+        height: 250,
+        width: 250,
+        child: CustomPaint(
+          painter: Painter(rectArr, uiImage),
+        ),
+      ),
+    );
+  }
+
+
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       backgroundColor: CupertinoColors.white,
       appBar: AppBar(
         backgroundColor: Colors.transparent,
       ),
       body: Container(
-        child: Text("TEST"),
+        child: Column(
+          children: [
+            Center(
+              child: TextButton(
+                onPressed: () { pickImage(); },
+                child: Text("TEST"),
+              ),
+            ),
+            Center(
+              child: Container(
+                width: 250,
+                height: 250,
+                child: null,
+              ),
+            )
+
+          ],
+        ),
       ),
     );
   }
